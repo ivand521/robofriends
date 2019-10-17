b4e25e29be5f8e66375675fafbcd5523382929e2
ivand521 <ivandong521@gmail.com>
2019-10-17 19:03:41 -0400

    Updates

---
+++ precache-manifest.cb76a48e8a82a7f50e1226999727ecd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd010be962b1d7be2592466fd7ff6ab",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "43eebc99455f83d3b7ed",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "01b0e203f02069584b38",
+    "url": "/robofriends/static/css/main.24d6feb6.chunk.css"
+  },
+  {
+    "revision": "43eebc99455f83d3b7ed",
+    "url": "/robofriends/static/js/2.46e1acfb.chunk.js"
+  },
+  {
+    "revision": "01b0e203f02069584b38",
+    "url": "/robofriends/static/js/main.336240b4.chunk.js"
+  },
+  {
+    "revision": "b27860c694e597dc925a",
+    "url": "/robofriends/static/js/runtime-main.d67d65c1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
